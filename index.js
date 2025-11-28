e1c18dd9301686610194c987d696c780f0e9e7cb
Chandru s <chandrusadhasivam04@gmail.com>
2025-11-28 10:17:12 +0530

    initial commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 // ===============================
 app.use(
   cors({
-    origin: "http://localhost:5173", // React frontend (Vite default)
+    origin: "https://book-verse-amber.vercel.app/", // React frontend (Vite default)
     credentials: true,
   })
 );
